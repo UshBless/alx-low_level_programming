148320f9b4c142392d1d1d3974e206157204c97a
UshBless <ndukablessing091@gmail.com>
2023-04-18 08:30:51 -0700

    Task 3

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "0. Object-like Macro"
+#include "1. Pi"
+#include "2. File name"
+#include "3. Function-like macro"
+#include "4. SUM"
+
+#endif
